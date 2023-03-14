5fe4839d06168c9c7cbe7c77b16f050363e4cfed
Henry <henrysungyun@gmail.com>
2023-03-13 20:55:45 -0700

    finish

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    c = 10
+    while c > 0:
+        print(c)
+        c -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []
+    for num in int_list:
+        if type(num) == int:
+            squared_list.append(num ** 2)
+    return squared_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for p in range(1,101):
+        if int(p/3) == p/3 and int(p/5) == p/5:
+            print("FizzBuzz")
+        elif int(p/3) == p/3:
+            print("Fizz")
+        elif int(p/5) == p/5:
+            print("Buzz")
+        else:
+            print(p)
